87111348700fc90a7793d85707fe0e4f4758449e
razielar <razielar@gmail.com>
2024-06-29 17:00:25 +0200

    error metrics exercise done

--- src/error_metrics.py
+++ src/error_metrics.py
@@ -16,3 +16,17 @@ def forecast_bias(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     sum_y = np.sum(y_true)
     return sum_errors / sum_y
 
+def WAPE(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """WAPE: Weighted mean Absolute Percentage Error.
+    WAPE = sum(abs(error))/sum(abs(true value))
+    If the data has trend, we should not use WAPE.
+    Args:
+        y_true (np.ndarray): true value
+        y_pred (np.ndarray): forecast
+    Returns:
+        float: WAPE
+    """
+    sum_errors = np.sum(np.abs(y_true - y_pred))
+    sum_abs_y = np.sum(np.abs(y_true))
+    return sum_errors / sum_abs_y
+
